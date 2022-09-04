c2b59f17121cb9298cd2ed786a38a79d025b7c75
ifhito <hito01010101@gmail.com>
2022-09-04 23:49:09 +0900

    ツールチップスのグループを括るコンポーネント

---
+++ src/components/Molecules/ToolTipsGroup.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import { ref, defineProps  } from 'vue'
+    import Button from '../Atoms/Button.vue';
+    interface Props {
+        tooltipsList: {
+            onClick: () => void;
+            iconName: string; 
+        }[]
+    }
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <div class="toolGroup">
+        <Button v-for="tooltips in tooltipsList" v-bind:key="tooltips.iconName" :onClick="tooltips.onClick">
+            <font-awesome-icon :icon="tooltips.iconName"/>
+        </Button>
+    </div>
+</template>
+
+<style scoped>
+</style>
+    
