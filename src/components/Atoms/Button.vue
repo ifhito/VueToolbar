d9078bd7ce518b7ab819de6066872c373c12df21
ifhito <hito01010101@gmail.com>
2022-09-04 23:48:54 +0900

    Button要素の作成

---
+++ src/components/Atoms/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import { ref, defineProps  } from 'vue'
+    interface Props {
+        onClick: () => void;
+    }
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <button @click="() => onClick()">
+    <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
+    
