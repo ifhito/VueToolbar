f3dfeae028c625cc8da86e653c63f4593e724acd
ifhito <hito01010101@gmail.com>
2022-09-04 23:49:24 +0900

    ツールバー全体のコンポーネント

---
+++ src/components/Organisms/ToolBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { ref, defineProps  } from 'vue'
+    import Button from '../Atoms/Button.vue';
+import ToolTipsGroup from '../Molecules/ToolTipsGroup.vue';
+    interface Props {
+        toolBarList: {
+            GroupName: string;
+            tooltipsGroupList: {
+                onClick: () => void;
+                iconName: string;
+            }[];
+        }[];
+    }
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <div class="toolbar" >
+        <ToolTipsGroup v-for="toolBar in toolBarList" v-bind:key="toolBar.GroupName" :tooltips-list="toolBar.tooltipsGroupList" />
+    </div>
+</template>
+
+<style scoped>
+.toolbar {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+}
+</style>
+    
