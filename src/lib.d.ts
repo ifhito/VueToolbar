599f685f0781735f0512ce1558f3bfed894a8be6
ifhito <hito01010101@gmail.com>
2022-09-10 12:53:21 +0900

    fixed: 型の変更

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,18 +1,14 @@
-export type tooltipType = {
-  onClick: (disabled: boolean | null) => void;
-  tabindex: number;
-  iconName: string;
-  tooltipText: string;
-};
-
 export type areaPropaties = {
   pressed: boolean | null;
   disabled: boolean | null;
 };
 
-export type tooltipsGroupType = {
+export type tooltipType = {
   onClick: (disabled: boolean | null) => void;
   tabindex: number;
   iconName: string;
+  tooltipText: string;
   areaPropaties: areaPropaties;
-}[];
+};
+
+export type tooltipsGroupType = tooltipType[];
